c1bded4085e4e34327b1eb84531a01d10488ec9c
Carolina943 <akamifox943@gmail.com>
2022-07-18 10:16:36 -0400

    El archivo login.js se modifico

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function  validate(){
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  if(username === "Patricia" && password === "admin"){
+  if(username === "name" && password === "admin"){
     alert("Login Successfuly!")
 }else{
   alert("Login failed!")
